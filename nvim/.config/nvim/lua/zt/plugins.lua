50b62ef34e96c630f88ef3b2026b9a97c9f58a6b
Zett Moonshine <zett@example.com>
2023-08-14 05:36:31 +0200

    feat: install telescope

--- nvim/.config/nvim/lua/zt/plugins.lua
+++ nvim/.config/nvim/lua/zt/plugins.lua
@@ -1,5 +1,9 @@
 local plugins = {
-  "ThePrimeagen/vim-be-good"
+	"ThePrimeagen/vim-be-good",
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
 }
 
 local opts = {
@@ -8,14 +12,14 @@ local opts = {
 -- Bootstrap code for Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 

d473583105687d041607b16454d07c24ab204bef
Zett Moonshine <zett@example.com>
2023-08-14 23:02:01 +0200

    docs: add a short description for each package installed

--- nvim/.config/nvim/lua/zt/plugins.lua
+++ nvim/.config/nvim/lua/zt/plugins.lua
@@ -22,7 +22,10 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
+	-- VimBeGood - Learn vim motions using games
 	use "ThePrimeagen/vim-be-good"
+
+	-- Telescope - Fuzzy finder
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		requires = { { 'nvim-lua/plenary.nvim' } },

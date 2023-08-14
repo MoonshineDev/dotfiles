7fd9020ce74f43ba9f32847c8619940c621ff2d0
Zett Moonshine <zett@example.com>
2023-08-14 22:58:00 +0200

    feat: migrate to packer.nvim

--- nvim/.config/nvim/lua/zt/plugins.lua
+++ nvim/.config/nvim/lua/zt/plugins.lua
@@ -1,27 +1,31 @@
-local plugins = {
-	"ThePrimeagen/vim-be-good",
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	}
-}
-
-local opts = {
-}
-
--- Bootstrap code for Lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+function bootstrap()
+	-- Ensure that packer.nvim has been cloned into ~/.local
+	local packerpath = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if not vim.loop.fs_stat(packerpath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--depth=1",
+			"https://github.com/wbthomason/packer.nvim.git",
+			packerpath,
+		})
+	end
+	vim.opt.rtp:prepend(packerpath)
 end
-vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
+bootstrap()
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use "ThePrimeagen/vim-be-good"
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		requires = { { 'nvim-lua/plenary.nvim' } },
+	}
+end)
 

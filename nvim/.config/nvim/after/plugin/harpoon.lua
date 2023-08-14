4f143ea311c174c3ba6211a45eb60f7b9edc276b
Zett Moonshine <zett@example.com>
2023-08-15 01:18:41 +0200

    docs: add descriptions to harpoon nav keybinds

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -4,8 +4,8 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>ba", mark.add_file, { desc = "Bookmark Add: file to bookmark list" })
 vim.keymap.set("n", "<leader>bl", ui.toggle_quick_menu, { desc = "Bookmark List: show all bookmarked files" })
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = "Bookmark Open 1: open file 1 in bookmark list" })
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, { desc = "Bookmark Open 2: open file 2 in bookmark list" })
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, { desc = "Bookmark Open 3: open file 3 in bookmark list" })
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Bookmark Open 4: open file 4 in bookmark list" })
 

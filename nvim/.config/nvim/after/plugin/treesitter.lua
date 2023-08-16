b0555b0fc7c20e35da25d833c85f7b226d38e2b0
Zett Moonshine <zett@example.com>
2023-08-16 22:15:34 +0200

    refactor: format list of parsers

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,10 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc" },
+  ensure_installed = {
+	  "lua",
+	  "vim",
+	  "vimdoc",
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

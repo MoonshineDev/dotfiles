d9f1063f6dcc64ff159cf7b191c96cf766ad8132
Zett Moonshine <zett@example.com>
2023-08-17 23:55:45 +0200

    feat: install LSP Zero

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -48,5 +48,22 @@ return require('packer').startup(function(use)
 
 	-- Fugitive - Git wrapper
 	use "tpope/vim-fugitive"
+
+	-- LSP Zero - Boilerplate fo a working LSP setup
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
 end)
 

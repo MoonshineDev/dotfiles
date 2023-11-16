c86d336b5dc02c7bbfeb8c2e6b36098f6d82ddb1
Zett Moonshine <zett@example.com>
2023-11-16 23:09:03 +0100

    fix(zsh): alias gapan should only add

--- zsh/.config/zsh/plugin-git.sh
+++ zsh/.config/zsh/plugin-git.sh
@@ -1,4 +1,4 @@
-alias gapan='git add --intent-to-add . && git commit --patch'
+alias gapan='git add --intent-to-add . && git add --patch'
 alias gcam!='git commit --all --amend --message'
 alias gpf!='git push --force'
 

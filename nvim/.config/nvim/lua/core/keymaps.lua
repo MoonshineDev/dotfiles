afb0acb69de6460fec9665360d70197007f6de99
Zett Moonshine <zett@example.com>
2024-06-01 01:00:06 +0200

    feat(nvim): map ' n' to :NvimTreeToggle

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
+-- Native file explorer (netrw)
 vim.keymap.set("n", "<Leader>e", ":Explore<cr>")
+-- Plugin nvim-tree
+vim.keymap.set("n", "<Leader>n", ":NvimTreeToggle<cr>")
 

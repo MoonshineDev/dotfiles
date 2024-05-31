bbcdc83cf006a9e42011dd28c46c74f4cf77f10d
Zett Moonshine <zett@example.com>
2024-06-01 01:13:57 +0200

    feat(nvim): map window management

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 -- Native file explorer (netrw)
 vim.keymap.set("n", "<Leader>e", ":Explore<cr>")
+-- Native window management
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 -- Plugin nvim-tree
 vim.keymap.set("n", "<Leader>n", ":NvimTreeToggle<cr>")
 

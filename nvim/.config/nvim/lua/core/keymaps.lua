8a07b33d7ae44f5f0cb04aef987f45bd9b7ae9dd
Zett Moonshine <zett@example.com>
2024-10-21 22:53:31 +0200

    feat(nvim): disable C-h and C-l keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 -- Native file explorer (netrw)
 vim.keymap.set("n", "<Leader>e", ":Explore<cr>")
 -- Native window management
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+--vim.keymap.set("n", "<C-h>", "<C-w>h")
+--vim.keymap.set("n", "<C-l>", "<C-w>l")
 -- Plugin nvim-tree
 if _G.use_nvim_tree then
 	vim.keymap.set("n", "<Leader>n", ":NvimTreeToggle<cr>")

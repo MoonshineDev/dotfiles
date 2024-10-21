ca9fe86132490769853c5dee80df4174d9c95669
Zett Moonshine <zett@example.com>
2024-10-21 23:26:17 +0200

    feat(nvim): group tab management keymaps under ' t'

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -4,6 +4,12 @@ vim.keymap.set("n", "<Leader>e", ":Explore<cr>")
 -- Native window management
 --vim.keymap.set("n", "<C-h>", "<C-w>h")
 --vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- Native tab management
+vim.keymap.set("n", "<Leader>tt", ":tabnew<cr>")
+vim.keymap.set("n", "<Leader>tw", ":tabclose<cr>")
+vim.keymap.set("n", "<Leader>tW", ":tabonly<cr>")
+vim.keymap.set("n", "<Leader>th", ":tabnext -<cr>")
+vim.keymap.set("n", "<Leader>tl", ":tabnext +<cr>")
 -- Plugin nvim-tree
 if _G.use_nvim_tree then
 	vim.keymap.set("n", "<Leader>ft", ":NvimTreeToggle<cr>")

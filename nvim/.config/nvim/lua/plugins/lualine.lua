a02d161866876c17659b6e38af08f8ee5523c49d
Zett Moonshine <zett@example.com>
2024-10-23 22:32:05 +0200

    feat(nvim): start lualine by default

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,9 @@ end
 
 return {
 	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' }
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	config = function()
+		require('lualine').setup()
+	end
 }
 

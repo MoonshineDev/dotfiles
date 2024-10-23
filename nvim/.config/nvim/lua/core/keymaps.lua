1b5e7dc778f31bb200261b753d4be00ca347a7bf
Zett Moonshine <zett@example.com>
2024-10-23 22:06:44 +0200

    feat(nvim): move nvim-tree keymaps to plugins

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -10,10 +10,4 @@ vim.keymap.set("n", "<Leader>tw", ":tabclose<cr>")
 vim.keymap.set("n", "<Leader>tW", ":tabonly<cr>")
 vim.keymap.set("n", "<Leader>th", ":tabnext -<cr>")
 vim.keymap.set("n", "<Leader>tl", ":tabnext +<cr>")
--- Plugin nvim-tree
-if _G.use_nvim_tree then
-	vim.keymap.set("n", "<Leader>ft", ":NvimTreeToggle<cr>")
-	vim.keymap.set("n", "<Leader>fo", ":NvimTreeOpen<cr>")
-	vim.keymap.set("n", "<Leader>fc", ":NvimTreeClose<cr>")
-end
 
--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -2,6 +2,10 @@ if not _G.use_nvim_tree then
 	return {}
 end
 
+vim.keymap.set("n", "<Leader>ft", ":NvimTreeToggle<cr>")
+vim.keymap.set("n", "<Leader>fo", ":NvimTreeOpen<cr>")
+vim.keymap.set("n", "<Leader>fc", ":NvimTreeClose<cr>")
+
 return {
 	"nvim-tree/nvim-tree.lua",
 	version = "*",

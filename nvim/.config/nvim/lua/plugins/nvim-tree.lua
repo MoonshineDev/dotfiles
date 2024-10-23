68c3666b73c214c61165f17718c66d7b612f0fbf
Zett Moonshine <zett@example.com>
2024-10-23 22:05:54 +0200

    refactor(nvim): simplify nvim-tree config

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,16 +1,16 @@
 if not _G.use_nvim_tree then
 	return {}
-else
-	return {
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup {}
-		end,
-	}
 end
+
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup {}
+	end,
+}
 

5f576ef47cb1a3a658be828792d161bb6e60ec4b
Zett Moonshine <zett@example.com>
2024-10-24 00:49:04 +0200

    docs(nvim): add desc to leader-e

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 -- Native file explorer (netrw)
-vim.keymap.set("n", "<Leader>e", ":Explore<cr>")
+vim.keymap.set("n", "<Leader>e", ":Explore<cr>", { desc = "Native file explorer (netrw)" })
 -- Native window management
 --vim.keymap.set("n", "<C-h>", "<C-w>h")
 --vim.keymap.set("n", "<C-l>", "<C-w>l")

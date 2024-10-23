b71563f836dfab8807d2629f387f833deae515db
Zett Moonshine <zett@example.com>
2024-10-23 23:01:24 +0200

    feat(nvim): install gitsigns

--- nvim/.config/nvim/lua/plugin-manager/lazy/config.lua
+++ nvim/.config/nvim/lua/plugin-manager/lazy/config.lua
@@ -2,6 +2,8 @@
 _G.use_nvim_tree = false
 -- status bar at bottom of window
 _G.use_lualine = true
+-- show git changes next to line number
+_G.use_gitsigns = true
 
 require("lazy").setup("plugins")
 
---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+if not _G.use_gitsigns then
+	print "Skipping plugin: gitsigns"
+	return {}
+end
+
+return {
+	'lewis6991/gitsigns.nvim',
+	opts = {
+		signs = {
+			add = { text = '+' },
+			change = { text = '~' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '~' },
+		},
+	}
+}
+

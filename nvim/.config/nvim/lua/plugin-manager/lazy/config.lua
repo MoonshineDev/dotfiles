7e5376892cbb384a8b3fe1d5fe8edaa191139821
Zett Moonshine <zett@example.com>
2024-10-23 23:10:34 +0200

    feat(nvim): order toggle flags by name

--- nvim/.config/nvim/lua/plugin-manager/lazy/config.lua
+++ nvim/.config/nvim/lua/plugin-manager/lazy/config.lua
@@ -1,9 +1,9 @@
--- file explorer, replaces netrw
-_G.use_nvim_tree = false
--- status bar at bottom of window
-_G.use_lualine = true
 -- show git changes next to line number
 _G.use_gitsigns = true
+-- status bar at bottom of window
+_G.use_lualine = true
+-- file explorer, replaces netrw
+_G.use_nvim_tree = false
 
 require("lazy").setup("plugins")
 
